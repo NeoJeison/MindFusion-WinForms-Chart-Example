0d74d24010dde939e50139949fd3effcf9b12da1
NeoJeison <jeyson7@outlook.com>
2019-03-18 13:26:42 -0500

    MindFusion Chart example

---
+++ Graficador.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+using MindFusion.Charting.WinForms;
+using MindFusion.Charting;
+using MindFusion.Charting.Components;
+
+namespace Graficador
+{
+    class Graficador
+    {
+        public static readonly string RUTA = "D:/Image.jpg";
+        public static readonly string NOMBRE_EJE_X = "# Elements";
+        public static readonly string NOMBRE_EJE_Y = "Distance (m)";
+        public static readonly int ANCHO = 1400;
+        public static readonly int ALTO = 900;
+
+        private LineChart chart;
+
+        public Graficador()
+        {
+            chart = new LineChart();
+            chart.XAxis.Title = NOMBRE_EJE_X;
+            chart.YAxis.Title = NOMBRE_EJE_Y;
+            chart.Theme = new Theme();
+            chart.Theme.CommonSeriesFills = chart.Theme.CommonSeriesStrokes = new List<MindFusion.Drawing.Brush>
+            {
+                new MindFusion.Drawing.SolidBrush(Color.Orange),
+                new MindFusion.Drawing.SolidBrush(Color.Purple),
+                new MindFusion.Drawing.SolidBrush(Color.Green),
+                new MindFusion.Drawing.SolidBrush(Color.Blue),
+                new MindFusion.Drawing.SolidBrush(Color.Red)
+            };
+            chart.GridType = GridType.Horizontal;
+            chart.Theme.GridLineColor = Color.White;
+            chart.Theme.GridColor1 = Color.White;
+            chart.Theme.GridColor2 = Color.White;
+            chart.ShowLegendTitle = false;
+            chart.Width = ANCHO;
+            chart.Height = ALTO;
+            chart.LegendHorizontalAlignment = LayoutAlignment.Center;
+            chart.LegendVerticalAlignment = LayoutAlignment.Near;
+        }
+
+        public void AgregarSerie(string nombreSerie, IList<double> listaX, IList<double> listaY)
+        {
+            Series2D serie = new Series2D(listaX, listaY, null);
+            serie.Title = nombreSerie;
+            chart.Series.Add(serie);
+        }
+
+        public void GuardarGrafica()
+        {
+            chart.CreateImage();
+            chart.ExportImage(RUTA);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graficador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] xs = { 0.0, 1.0, 2.0, 3.0, 4.0 };
+            double[] ys = { 3.34, 3.29, 2.5, 2.25, 1, 8 };
+
+            double[] xz = { 0.0, 1.0, 2.0, 3.0, 4.0 };
+            double[] yz = { 3.47, 2.73, 2.8, 2.32, 1.8 };
+
+            double[] xc = { 0.0, 1.0, 2.0, 3.0, 4.0 };
+            double[] yc = { 3.4, 3.25, 2.75, 2.25, 1.82 };
+
+            Graficador graficador = new Graficador();
+
+            graficador.AgregarSerie("Left", xs, ys);
+            graficador.AgregarSerie("Right", xz, yz);
+            graficador.AgregarSerie("Center", xc, yc);
+            graficador.GuardarGrafica();
+            //Console.ReadKey();
+        }
+    }
+}
